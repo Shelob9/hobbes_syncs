38030a091002bba302c9613acff33481cb4951a2
Josh Pollock <jpollock412@gmail.com>
2014-12-28 18:29:37 -0700

    inline docs

--- classes/jp_keyed_request/src/functions.php
+++ classes/jp_keyed_request/src/functions.php
@@ -1,26 +1,31 @@
 <?php
 /**
- * @TODO What this does.
+ * Functions for this lib
  *
- * @package   @TODO
+ * @package   @jp_keyed_access
  * @author    Josh Pollock <Josh@JoshPress.net>
  * @license   GPL-2.0+
  * @link      
  * @copyright 2014 Josh Pollock
  */
 
+/**
+ * Set access to pods API if request has right key/token
+ *
+ * @since 0.0.1
+ */
 add_action( 'init', function() {
-
-		$jp_keyed_request_auth = jp_keyed_request\auth\verify::allow_access();
-		if ( ! is_wp_error( $jp_keyed_request_auth) && $jp_keyed_request_auth ) {
-			add_filter('pods_json_api_access_pods_save_item', '__return_true' );
-		}
-
+	$jp_keyed_request_auth = jp_keyed_request\auth\verify::allow_access();
+	if ( ! is_wp_error( $jp_keyed_request_auth) && $jp_keyed_request_auth ) {
+		add_filter('pods_json_api_access_pods_save_item', '__return_true' );
+	}
 
 });
 
 /**
  * Make a request with key args
+ *
+ * @since 0.0.1
  *
  * @param string $url Url to make request
  * @param array|object|JSON $data Body of request
@@ -33,7 +38,4 @@ function jp_keyed_request_make( $url, $data, $method = 'POST' ) {
 
 	return $make->make();
 }
-
-
-add_filter('pods_json_api_access_pods_save_item', '__return_true' );
 
